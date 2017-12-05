b540ff1f5d955bd31a1f926cbeec201935b4668e
Hans Buys <buyshans@gmail.com>
2017-12-04 22:24:49 -0500

    reorganized findbystatehandler createReply for less indenting.

--- front-end/src/service/intent/findByState.ts
+++ front-end/src/service/intent/findByState.ts
@@ -16,31 +16,40 @@ export class FindByStateHandler extends HandlerBase {
     }
 
     public async createReply(params?: DictionaryOfStrings): Promise<string | false> {
-        if (params && params["state"]) {
-            const state = params["state"].value;
-
-            if (!state) {
-                throw new Error("No state defined.");
-            }
-
-            const schoolsInState = await this.schoolRepo.getForState(state);
-            const numberOfSchoolsInState = schoolsInState.length;
-
-            let returnText = `Found ${numberOfSchoolsInState} schools for state ${state}.`;
-            if (numberOfSchoolsInState > this.maxNumberOfSchoolsToReturn) {
-                returnText += `\nReturning the first ${this.maxNumberOfSchoolsToReturn} alphabetically sorted:`;
-            } else {
-                returnText += "\nHere's the list:";
-            }
-
-            schoolsInState
-                .sort((first, second) => first.name.localeCompare(second.name))
-                .slice(0, 5)
-                .forEach((school) => returnText += `\n - ${school.name}`);
-
-            return returnText;
+        if (!params || !params["state"]) {
+            return false;
         }
 
-        return false;
+        const state = params["state"].value;
+
+        if (!state) {
+            throw new Error("No state defined.");
+        }
+
+        const schoolsInState = await this.schoolRepo.getForState(state);
+        const numberOfSchoolsInState = schoolsInState.length;
+
+        if (numberOfSchoolsInState === 0) {
+            return `No schools found in state ${state}.`;
+        }
+
+        let returnText = `Found ${numberOfSchoolsInState} schools for state ${state}.`;
+
+        if (numberOfSchoolsInState > this.maxNumberOfSchoolsToReturn) {
+            returnText += `\nReturning the first ${this.maxNumberOfSchoolsToReturn} alphabetically sorted:`;
+        } else {
+            returnText += "\nHere's the list:";
+        }
+
+        schoolsInState
+            .sort((first, second) => first.name.localeCompare(second.name))
+            .slice(0, 5)
+            .forEach((school) => returnText += `\n - ${school.name}`);
+
+        if (numberOfSchoolsInState > this.maxNumberOfSchoolsToReturn) {
+            returnText += `\n - ...`;
+        }
+
+        return returnText;
     }
 }
